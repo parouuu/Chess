d8bb82a2106af220775140679e4f7ea62a1a6839
parouuu <stephverez@free.fr>
2013-12-14 15:40:55 +0000

    transparency for moves added

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -16,6 +16,7 @@ public class Chess extends JPanel implements MouseListener {
 	/** constructors **/
 	public Chess() {
 		this.background = new ImageIcon("Image/chessboard.jpg").getImage();
+		this.transparency = new ImageIcon("Image/transparency.png").getImage();
 		initGame();
 		addMouseListener(this);
 		this.setMinimumSize(new Dimension(641, 641));
@@ -60,14 +61,14 @@ public class Chess extends JPanel implements MouseListener {
 		board[7][5] = new piece(new pos(7,5), "bishop", true, new ImageIcon("Image/WhiteBishop.png").getImage());
 		board[7][6] = new piece(new pos(7,6), "knight", true, new ImageIcon("Image/WhiteHorse.png").getImage());
 		board[7][7] = new piece(new pos(7,7), "rook", true, new ImageIcon("Image/WhiteTower.png").getImage());
-		board[6][0] = new piece(new pos(6,0), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
+		/*board[6][0] = new piece(new pos(6,0), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
 		board[6][1] = new piece(new pos(6,1), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
 		board[6][2] = new piece(new pos(6,2), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
 		board[6][3] = new piece(new pos(6,3), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
 		board[6][4] = new piece(new pos(6,4), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
 		board[6][5] = new piece(new pos(6,5), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
 		board[6][6] = new piece(new pos(6,6), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
-		board[6][7] = new piece(new pos(6,7), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());
+		board[6][7] = new piece(new pos(6,7), "pawn", true, new ImageIcon("Image/WhitePawn.png").getImage());*/
 	}
 	
 	// repaints the widget when an update of any kind is made
@@ -76,6 +77,7 @@ public class Chess extends JPanel implements MouseListener {
 			drawPieces(g2d);
 			g.drawImage(this.background, 0, 0, null);
 			drawPieces(g);
+			printTransparency(g);
 		}
 	
 	private void drawPieces(Graphics g) {
@@ -107,6 +109,16 @@ public class Chess extends JPanel implements MouseListener {
 		
 	}
 
+	void printTransparency(Graphics g) {
+		if (pl != null) {
+			for (pos p : pl)
+			{
+				System.out.print("\npos = (" + p.getX() + ";" + p.getY() + ")");
+				g.drawImage(transparency, (p.getY() * (675 / 8)) + 50, (p.getX() * (682 / 8)) + 49, null);
+			}
+		}
+	}
+	
 	@Override
 	public void mousePressed(MouseEvent event) {
 		if (event.getButton() == MouseEvent.BUTTON1 && (event.getX() - 48) > 0 && (event.getX() < (44 + 675)) && (event.getY() - 47) > 0 && event.getY() < (682 + 44)) {		// if left button is pressed and the clic is in the board limits
@@ -115,12 +127,8 @@ public class Chess extends JPanel implements MouseListener {
 			oldposx = event.getX();
 			oldposy = event.getY();
 			if (board[oldy][oldx] != null && board[oldy][oldx].getPlayer() == player ) {
-					ArrayList<pos> poslist = ref.checkMove(board[oldy][oldx], board.clone());
-					System.out.print("\nCLIC:(" + oldx + ";" + oldy + ")" + " = " + board[oldy][oldx].name + "| pos piece = " + board[oldy][oldx].getPos().getX() + ":" + board[oldy][oldx].getPos().getY());
-					for (pos p : poslist)
-						{
-							System.out.print("\npos = (" + p.x + ";" + p.y + ")");
-						}
+					pl = ref.checkMove(board[oldy][oldx], board.clone());
+					System.out.print("\nCLIC:(" + oldy + ";" + oldx + ")" + " = " + board[oldy][oldx].name + " | position piece = " + board[oldy][oldx].getPos().getX() + ":" + board[oldy][oldx].getPos().getY());
 				}
 			}
 		repaint();
@@ -136,7 +144,8 @@ public class Chess extends JPanel implements MouseListener {
 	piece board[][];
 	int oldx, oldy;		// denotes where the player clicked when he pressed the mouse button
 	int oldposx, oldposy;
-	Image background;
+	Image background, transparency;
 	boolean player;
 	referee ref;
+	ArrayList<pos> pl; // List of possible moves for the last piece clicked
 }
