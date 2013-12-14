8563b55c54095b5f978be0a6e45908d6ba9332e7
Raphael Novembrino <raphael.novembrino@epitech.eu>
2013-12-14 12:47:06 +0000

    debut de referee

--- src/chess/referee.java
+++ src/chess/referee.java
@@ -11,8 +11,23 @@ public class referee {
 
 	public ArrayList<pos> checkMove(piece toMove, piece board[][])
 	{
-		return null;
+		piece	cloneBoard[][] = board.clone();
+		ArrayList<ArrayList<pos>> tmp = toMove.checkMove();
+		ArrayList<pos> ret;
 		
+		ret = this.cutView(tmp, cloneBoard);
+		this.cutMate(ret, cloneBoard);
+		return ret;
+	}
+
+	private void cutMate(ArrayList<pos> ret, piece[][] cloneBoard) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private ArrayList<pos> cutView(ArrayList<ArrayList<pos>> tmp, piece[][] cloneBoard) {
+		
+		return null;
 	}
 	
 }
